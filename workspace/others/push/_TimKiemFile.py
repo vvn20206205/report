52ee9f5abbc1f93ae7b7abd02ad74d4b23016ece
Vũ Văn Nghĩa 20206205 <nghiavu2k2abc@gmail.com>
2024-01-04 09:15:48 +0700

    VuVanNghia20206205

--- workspace/others/push/_TimKiemFile.py
+++ workspace/others/push/_TimKiemFile.py
@@ -2,11 +2,11 @@ import os
 import glob
 root_dir = os.path.join(os.getcwd(), r"..\\..\\..\\")
 # root_dir=r"c:\Users\vvn20206205\Pictures"
-ext=".py"
-ext=".srt"
-ext=".mp4"
-ext=".md"
+# ext=".srt"
+# ext=".mp4"
+# ext=".py"
 ext=".tex"
+ext=".md"
 files=glob.glob(os.path.join(  root_dir, f'**/*{ext}'), recursive=True) 
 print(len(files))
 for f in files:
